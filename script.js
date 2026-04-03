5b2615d5d676f50b70d757f34f893ac748926c7c
Arhchu <71408816+abaskarasingham@users.noreply.github.com>
2026-04-03 18:44:55 -0400

    Add start button and feature to name players
    The players can start the game with this button,
    and enter their desired names which will be
    displayed in the game.

--- script.js
+++ script.js
@@ -28,9 +28,10 @@ function createPlayer(name, marker) {
     const setMarker = (marker) => m_marker = marker;
 
     const getScore = () => m_score;
+    const setScore = (score) => m_score = score;
     const giveScore = () => ++m_score;
 
-    return { getName, setName, getMarker, setMarker, getScore, giveScore };
+    return { getName, setName, getMarker, setMarker, getScore, setScore, giveScore };
 };
 
 const DisplayController = (() => {
@@ -38,12 +39,26 @@ const DisplayController = (() => {
   const message = document.querySelector(".message");
   const p1score = document.querySelector(".p1score");
   const p2score = document.querySelector(".p2score");
+  const gameForm = document.querySelector("#game-dialog form");
   const resetBtn = document.querySelector(".resetBtn");
-    resetBtn.addEventListener("click", function(e) {
-        GameBoard.reset();
-        GameController.reset();
-        render();
-    });
+
+  gameForm.addEventListener("submit", function(e) {
+    const player1 = document.querySelector("#player1");
+    const player2 = document.querySelector("#player2");
+    resetBtn.disabled = false;
+    GameController.startGame(player1.value, player2.value);
+    resetBtn.click();
+
+    player1.value = "";
+    player2.value = "";
+  });
+
+  resetBtn.disabled = true;
+  resetBtn.addEventListener("click", function (e) {
+    GameBoard.reset();
+    GameController.reset();
+    render();
+  });
 
   const render = () => {
     const board = GameBoard.getBoard();
@@ -76,65 +91,73 @@ const DisplayController = (() => {
     });
   });
 
-  return { render, displayWinner, displayTurn, displayTie, displayScore };
+  return { displayWinner, displayTurn, displayTie, displayScore };
 })();
 
 const GameController = (() => {
-    const player1 = createPlayer("Player1", 'X');
-    const player2 = createPlayer("Player2", 'O');
-    let currentPlayer = player1;
-    let gameOver = false;
-    DisplayController.displayScore(player1, player2);
+  const player1 = createPlayer("Player 1", 'X');
+  const player2 = createPlayer("Player 2", 'O');
+  let currentPlayer = player1;
+  let gameOver = true;
 
-    const playRound = (index) => {
-        if (gameOver) return;
+  const startGame = (p1Name, p2Name) => {
+    player1.setName(p1Name);
+    player1.setScore(0);
+    player2.setName(p2Name);
+    player2.setScore(0);
+    reset();
+  };
 
-        if (!GameBoard.setCell(index, currentPlayer.getMarker())) return;
+  const playRound = (index) => {
+    if (gameOver) return;
 
-        if (checkWin()) {
-            console.log(`${currentPlayer.getName()} Wins!`);
-            DisplayController.displayWinner(currentPlayer);
-            currentPlayer.giveScore();
-            gameOver = true;
-            DisplayController.displayScore(player1, player2);
-            return;
-        }
+    if (!GameBoard.setCell(index, currentPlayer.getMarker())) return;
 
-        if (checkTie()) {
-            console.log("It's a Tie!");
-            DisplayController.displayTie();
-            gameOver = true;
-            return;
-        }
-
-        switchPlayer();
-    };
-
-    const switchPlayer = () => {
-        currentPlayer = currentPlayer === player1 ? player2 : player1;
-        DisplayController.displayTurn(currentPlayer);
-    };
-
-    const checkWin = () => {
-        const board = GameBoard.getBoard();
-        const wins = [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8],
-            [0, 3, 6], [1, 4, 7], [2, 5, 8],
-            [0, 4, 8], [2, 4, 6],
-        ];
-
-        return wins.some(combo => combo.every(i => board[i] === currentPlayer.getMarker()));
-    };
-
-    const checkTie = () => {
-        return GameBoard.getBoard().every(cell => cell !== "");
-    };
-
-    const reset = () => {
-        currentPlayer = player1;
-        gameOver = false;
-        DisplayController.displayTurn(currentPlayer);
+    if (checkWin()) {
+      console.log(`${currentPlayer.getName()} Wins!`);
+      DisplayController.displayWinner(currentPlayer);
+      currentPlayer.giveScore();
+      gameOver = true;
+      DisplayController.displayScore(player1, player2);
+      return;
     }
 
-    return { playRound, reset };
+    if (checkTie()) {
+      console.log("It's a Tie!");
+      DisplayController.displayTie();
+      gameOver = true;
+      return;
+    }
+
+    switchPlayer();
+  };
+
+  const switchPlayer = () => {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+    DisplayController.displayTurn(currentPlayer);
+  };
+
+  const checkWin = () => {
+    const board = GameBoard.getBoard();
+    const wins = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6],
+    ];
+
+    return wins.some(combo => combo.every(i => board[i] === currentPlayer.getMarker()));
+  };
+
+  const checkTie = () => {
+    return GameBoard.getBoard().every(cell => cell !== "");
+  };
+
+  const reset = () => {
+    currentPlayer = player1;
+    gameOver = false;
+    DisplayController.displayTurn(currentPlayer);
+    DisplayController.displayScore(player1, player2);
+  }
+
+  return { startGame, playRound, reset };
 })();
